1de25e40ba91236e9c4ab627aeaebadc7cb471d6
mrnazu <samuelamsalu1998@gmail.com>
2024-05-14 00:34:09 -0700

    smart contract has been successfully deployed

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,10 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-ethers");
+
 module.exports = {
-  solidity: "0.8.24",
+    solidity: "0.8.0",
+    networks: {
+        localhost: {
+            url: "http://127.0.0.1:8545"
+        }
+    }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,17 @@
 async function main() {
+    const { ethers } = require("hardhat");
+
     const Calculator = await ethers.getContractFactory("Calculator");
     const calculator = await Calculator.deploy();
+
     await calculator.deployed();
+
     console.log("Calculator deployed to:", calculator.address);
 }
 
 main()
     .then(() => process.exit(0))
-    .catch(error => {
+    .catch((error) => {
         console.error(error);
         process.exit(1);
     });
